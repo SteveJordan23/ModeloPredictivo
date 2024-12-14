d56795d06906eaf4048577b4fffe3f95e6461e56
Jordan Steve <40286881+SteveJordan23@users.noreply.github.com>
2024-12-14 11:32:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -164,12 +164,12 @@ if model_executed:
 st.markdown(LOOKER_STUDIO_URL, unsafe_allow_html=True) 
 
 if model_executed:   
-# Enlace al reporte de Looker Studio
-    
-st.markdown(
-    """
-    Haz clic en el enlace para acceder al reporte de Looker Studio:
-    [Ver Reporte](https://lookerstudio.google.com/reporting/21028ef0-7f4c-4de2-bd41-97238c0e7951)
-    """
-)
+    # Enlace al reporte de Looker Studio
+        
+    st.markdown(
+        """
+        Haz clic en el enlace para acceder al reporte de Looker Studio:
+        [Ver Reporte](https://lookerstudio.google.com/reporting/21028ef0-7f4c-4de2-bd41-97238c0e7951)
+        """
+    )
 st.markdown(LOOKER_STUDIO_URL, unsafe_allow_html=True)
