bc14b56fd80401d32b1b14482986b5fda6232470
Jordan Steve <40286881+SteveJordan23@users.noreply.github.com>
2024-12-14 11:35:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -161,16 +161,11 @@ if model_executed:
         """,
         unsafe_allow_html=True
     )
-st.markdown(LOOKER_STUDIO_URL, unsafe_allow_html=True) 
 
-if model_executed:   
-
-    # Incrustar el iframe correctamente
-    LOOKER_STUDIO_URL = 'https://lookerstudio.google.com/embed/reporting/21028ef0-7f4c-4de2-bd41-97238c0e7951/page/MjRZE'
     st.markdown(
         """
         Haz clic en el enlace para acceder al reporte de Looker Studio:
         [Ver Reporte](https://lookerstudio.google.com/reporting/21028ef0-7f4c-4de2-bd41-97238c0e7951)
         """
     )
-st.markdown(LOOKER_STUDIO_URL, unsafe_allow_html=True)
+st.markdown(LOOKER_STUDIO_URL, unsafe_allow_html=True) 
